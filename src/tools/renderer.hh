ab580941c9222e5798af4102db6a593d0e46f376
Jason Liu <liujason2003@gmail.com>
2024-11-04 21:12:38 +0100

    add color mapping

--- src/tools/renderer.hh
+++ src/tools/renderer.hh
@@ -13,6 +13,37 @@
 
 #include <graphviz/gvc.h>
 
+typedef std::pair<std::string, std::string> stringpair_t;
+
+// Max support of 20 colors
+
+const stringpair_t map_start_values[] = {
+    stringpair_t("red", "1"),
+    stringpair_t("green", "2"),
+    stringpair_t("blue", "3"),
+    stringpair_t("yellow", "4"),
+    stringpair_t("orange", "5"),
+    stringpair_t("purple", "6"),
+    stringpair_t("brown", "7"),
+    stringpair_t("pink", "8"),
+    stringpair_t("gray", "9"),
+    stringpair_t("cyan", "10"),
+    stringpair_t("magenta", "11"),
+    stringpair_t("turquoise", "12"),
+    stringpair_t("gold", "13"),
+    stringpair_t("silver", "14"),
+    stringpair_t("black", "15"),
+    stringpair_t("white", "16"),
+    stringpair_t("maroon", "17"),
+    stringpair_t("lime", "18"),
+    stringpair_t("olive", "19"),
+    stringpair_t("navy", "20"),
+};
+
+const int map_start_values_size = sizeof(map_start_values) / sizeof(map_start_values[0]);
+
+const std::map<std::string, std::string> color_map (map_start_values, map_start_values + map_start_values_size);
+
 class Renderer {
 private:
     /* Helper functions */
